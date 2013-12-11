0c2f86a1d69c50b19a198c62061182e09ad24717
Davis, Morgan <morgan.davis@plantronics.com>
2013-12-11 15:00:33 -0800

    Updated PLTDevice for iOS to version 1.1.

---
+++ Desktop/OS X/MissileDodger/MissileDodger/MissileWindowController.h
@@ -0,0 +1,14 @@
+//
+//  MissileWindowController.h
+//  MissileDodger
+//
+//  Created by Davis, Morgan on 11/16/13.
+//  Copyright (c) 2013 Plantronics, Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface MissileWindowController : NSWindowController
+
+@end
