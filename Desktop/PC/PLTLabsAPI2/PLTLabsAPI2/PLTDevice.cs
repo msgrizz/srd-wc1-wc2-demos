8c69a56255c09ebe2718b76e9dbbcd0c17fc4912
lcollins <lewis.collins@plantronics.com>
2013-12-23 09:31:58 +0000

    PLTLabsAPI2 - BladeRunner version - has dependency on BRLibrary! (see below)
    This has dependency on BRLibrary, located in PerForce here:
    
    //plantronics/NPD/programs/badangle/PC/BRLibrary/

---
+++ Desktop/PC/PLTLabsAPI2/PLTLabsAPI2/PLTDevice.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Plantronics.Innovation.BRLibrary;
+
+namespace Plantronics.Innovation.PLTLabsAPI2
+{
+    /// <summary>
+    /// The PLTDevice object abstraction describes a device object
+    /// that is available for connecting to with the PLTLabsAPI
+    /// and ultimately subscribing for its services once the connection
+    /// is established.
+    /// </summary>
+    public class PLTDevice
+    {
+        /// <summary>
+        /// The Product Name of the device
+        /// </summary>
+        public string m_ProductName = "";
+        private HIDDevice m_HIDDevice;
+
+        // NOTE: commenting out below, because this is not currently available in
+        // Spokes 3.0 SDK / Spokes Wrapper
+        ///// <summary>
+        ///// The product base serial number
+        ///// </summary>
+        //public string m_ProductBaseSerial = "";
+        ///// <summary>
+        ///// The product headset serial number
+        ///// </summary>
+        //public string m_ProductHeadsetSerial = "";
+
+        public PLTDevice(HIDDevice aDevice)
+        {
+            m_ProductName = "";
+            m_HIDDevice = aDevice;
+        }
+    }
+}
---
+++ Desktop/PC/PLTLabsAPI2/PLTLabsAPI2/PLTInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Plantronics.Innovation.PLTLabsAPI2
+{
+    /// <summary>
+    /// The PLTInfo object abstration is used to pass data back to applications
+    /// for the various services that the app can subcribe to.
+    /// </summary>
+    public class PLTInfo
+    {
+        /// <summary>
+        /// This member holds the service type as PLTService enum value.
+        /// </summary>
+        public PLTService m_serviceType;
+
+        /// <summary>
+        /// This member is a object pointer that can be recast the appropriate
+        /// data type depending on the service type.
+        /// </summary>
+        public object m_data = null;
+
+        /// <summary>
+        /// The PLT default constructor
+        /// </summary>
+        /// <param name="serviceType">PLTService enum value</param>
+        /// <param name="data">object pointer to returned data</param>
+        public PLTInfo(PLTService serviceType, object data)
+        {
+            m_serviceType = serviceType;
+            m_data = data;
+        }
+    }
+}
