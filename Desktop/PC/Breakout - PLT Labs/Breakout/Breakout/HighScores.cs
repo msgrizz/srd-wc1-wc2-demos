6fff8a7e2b2acdd25dfa7ecd2a854dd171176f7c
lcollins <lewis.collins@plantronics.com>
2013-12-23 09:25:42 +0000

    Spokes 3.0 Breakout enhancements continued....
    See Game1.cs

---
+++ Desktop/PC/Breakout - PLT Labs/Breakout/Breakout/HighScores.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace Breakout
+{
+    public class HighScores
+    {
+        private string filename = "scores.xml";
+
+        public List<ScoreRecord> scores; //  to be sorted by score highest first
+          // sort using, e.g.: List<Order> SortedList = objListOrder.OrderBy(o=>o.OrderDate).ToList();
+
+        public HighScores()
+        {           
+            ReadScores();
+        }
+
+        public bool IsHighScore(int score)
+        {
+            bool ishighscore = false;
+
+            foreach (ScoreRecord rec in scores)
+            {
+                if (score > rec.score)
+                {
+                    ishighscore = true;
+                    break;
+                }
+            }
+
+            return ishighscore;
+        }
+
+        public void AddHighScore(string name, int score)
+        {
+            scores.Add(new ScoreRecord(name, score));
+            WriteScores();
+        }
+
+        private void ReadScores()
+        {
+            scores = new List<ScoreRecord>();
+            try
+            {
+                XElement xml = XElement.Load(filename);
+
+                bool found = false;
+                scores = new List<ScoreRecord>();
+                XElement HighScores = xml.XPathSelectElement("./HighScores");
+                if (HighScores != null)
+                {
+                    foreach (XElement item in HighScores.XPathSelectElements("./HighScore"))
+                    {
+                        ScoreRecord rec = new ScoreRecord();
+                        rec.name = (string)item.XPathSelectElement("./name");
+                        rec.score = (int)item.XPathSelectElement("./score");
+                        scores.Add(rec);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                // uh-oh, maybe there's no file yet!
+            }
+
+            // fill up with blanks
+            int n = scores.Count() - 1;
+            if (n < 0) n = 0;
+            for (int i = n; i < 10; i++)
+            {
+                ScoreRecord rec = new ScoreRecord("AAAAA", 0);
+                scores.Add(rec);
+            }
+        }
+
+        private void WriteScores()
+        {
+            // re-sort scores
+            List<ScoreRecord> tmpscores 
+                = scores.OrderByDescending(o=>o.score).ToList();
+            scores = new List<ScoreRecord>();
+            int n = 0;
+            foreach (ScoreRecord rec in tmpscores)
+            {
+                n++;
+                scores.Add(rec);
+                if (n == 10) break;
+            }
+
+            try
+            {
+                XElement xml = new XElement("breakoutdata");
+
+                XElement highScores = new XElement("HighScores");
+
+                // write up to 10 elements to file! (only track 10 top scores)
+                for (int i=0; i<10; i++)
+                {
+                    if (i < scores.Count() - 1)
+                    {
+                        ScoreRecord rec = scores[i];
+                        highScores.Add(new XElement("HighScore",
+                            new XElement("name", rec.name),
+                            new XElement("score", rec.score)));
+                    }
+                }
+
+                xml.Add(highScores);
+
+                xml.Save(filename);
+            }
+            catch (Exception)
+            {
+                // uh-oh
+            }
+        }    
+    }
+
+    public class ScoreRecord
+    {
+        public string name;
+        public int score;
+
+        public ScoreRecord(string name, int score)
+        {
+            this.name = name;
+            this.score = score;
+        }
+
+        public ScoreRecord()
+        {
+            this.name = "";
+            this.score = 0;
+        }
+    }
+}
