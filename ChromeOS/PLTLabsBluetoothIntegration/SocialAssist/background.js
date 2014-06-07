ddaef08ae6b727fadd970c0cf0e673d6aa538c67
Cary Bran <cary.bran@gmail.com>
2014-06-07 10:36:50 -0700

    Update background.js
    removed unused code and moved BR_PROFILE into js library for device access

--- ChromeOS/PLTLabsBluetoothIntegration/SocialAssist/background.js
+++ ChromeOS/PLTLabsBluetoothIntegration/SocialAssist/background.js
@@ -1,6 +1,4 @@
-var BR_PROFILE = {
-  uuid: '82972387-294e-4d62-97b5-2668aa35f618'
-};
+
 
 chrome.app.runtime.onLaunched.addListener(
 	function () {
@@ -10,26 +8,5 @@ chrome.app.runtime.onLaunched.addListener(
 		"bounds" : {
 			"width" : 1000,
 			"height" : 975 }
-		},
-		function(win) {
-	       // Add the profile to the list of profiles we support
-	       //chrome.bluetooth.addProfile(BR_PROFILE, function(r) {
-		//console.log("PLTLabs profile added");
-		//win.fullscreen();
-	       //});
-    
-	      // Make the profile available in the main content window.
-	      win.contentWindow.BR_PROFILE = BR_PROFILE;
-	      });
-	    
-	
-	});
-
-function removeProfile() {
-  console.log("Removing Bladerunner profile");
-  chrome.bluetooth.removeProfile(BR_PROFILE, function(r) {
-    console.log("Profile removed");
-  });
-}
-
-
+		},function(win) {});
+    });
